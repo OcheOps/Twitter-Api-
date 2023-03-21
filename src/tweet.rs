7d6e9be91f44cc4c9bff8c18b4c72429460c85fb
ochecodes7 <ochecodes@gmail.com>
2023-03-21 12:32:15 +0100

    tweets done calling

--- src/tweet.rs
+++ src/tweet.rs
@@ -1,83 +1,51 @@
-pub type Tweets = Response<Tweet>;
+fn list_tweets(total_tweets: i64, conn: &DBPooledConnection) -> Result<Tweets, Error> {
+    use crate::schema::tweets::dsl::*;
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Tweet {
-    pub id: String,
-    pub created_at: DateTime<Utc>,
-    pub message: String,
-    pub likes: Vec<Like>,
+    let _tweets = match tweets
+        .order(created_at.desc())
+        .limit(total_tweets)
+        .load::<TweetDB>(conn)
+    {
+        Ok(tws) => tws,
+        Err(_) => vec![],
+    };
+
+    Ok(Tweets {
+        results: _tweets
+            .into_iter()
+            .map(|t| t.to_tweet())
+            .collect::<Vec<Tweet>>(),
+    })
 }
 
-impl Tweet {
-    pub fn new(message: String) -> Self {
-        Self {
-            id: Uuid::new_v4().to_string(),
-            created_at: Utc::now(),
-            message,
-            likes: vec![],
-        }
+fn find_tweet(_id: Uuid, conn: &DBPooledConnection) -> Result<Tweet, Error> {
+    use crate::schema::tweets::dsl::*;
+
+    let res = tweets.filter(id.eq(_id)).load::<TweetDB>(conn);
+    match res {
+        Ok(tweets_db) => match tweets_db.first() {
+            Some(tweet_db) => Ok(tweet_db.to_tweet()),
+            _ => Err(Error::NotFound),
+        },
+        Err(err) => Err(err),
     }
 }
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct TweetRequest {
-    pub message: Option<String>,
+fn create_tweet(tweet: Tweet, conn: &DBPooledConnection) -> Result<Tweet, Error> {
+    use crate::schema::tweets::dsl::*;
+
+    let tweet_db = tweet.to_tweet_db();
+    let _ = diesel::insert_into(tweets).values(&tweet_db).execute(conn);
+
+    Ok(tweet_db.to_tweet())
 }
 
-impl TweetRequest {
-    pub fn to_tweet(&self) -> Option<Tweet> {
-        match &self.message {
-            Some(message) => Some(Tweet::new(message.to_string())),
-            None => None,
-        }
+fn delete_tweet(_id: Uuid, conn: &DBPooledConnection) -> Result<(), Error> {
+    use crate::schema::tweets::dsl::*;
+
+    let res = diesel::delete(tweets.filter(id.eq(_id))).execute(conn);
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err),
     }
-}
-
-/// list 50 last tweets `/tweets`
-#[get("/tweets")]
-pub async fn list() -> HttpResponse {
-    // TODO find the last 50 tweets and return them
-
-    let tweets = Tweets { results: vec![] };
-
-    HttpResponse::Ok()
-        .content_type(APPLICATION_JSON)
-        .json(tweets)
-}
-
-/// create a tweet `/tweets`
-#[post("/tweets")]
-pub async fn create(tweet_req: Json<TweetRequest>) -> HttpResponse {
-    HttpResponse::Created()
-        .content_type(APPLICATION_JSON)
-        .json(tweet_req.to_tweet())
-}
-
-/// find a tweet by its id `/tweets/{id}`
-#[get("/tweets/{id}")]
-pub async fn get(path: Path<(String,)>) -> HttpResponse {
-    // TODO find tweet a tweet by ID and return it
-    let found_tweet: Option<Tweet> = None;
-
-    match found_tweet {
-        Some(tweet) => HttpResponse::Ok()
-            .content_type(APPLICATION_JSON)
-            .json(tweet),
-        None => HttpResponse::NoContent()
-            .content_type(APPLICATION_JSON)
-            .await
-            .unwrap(),
-    }
-}
-
-/// delete a tweet by its id `/tweets/{id}`
-#[delete("/tweets/{id}")]
-pub async fn delete(path: Path<(String,)>) -> HttpResponse {
-    // TODO delete tweet by ID
-    // in any case return status 204
-
-    HttpResponse::NoContent()
-        .content_type(APPLICATION_JSON)
-        .await
-        .unwrap()
 }
