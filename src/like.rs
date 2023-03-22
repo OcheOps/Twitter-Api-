ada007c5fd28825a6d9f3e0ecb7bf389bfddeca6
ochecodes7 <ochecodes@gmail.com>
2023-03-22 23:44:29 +0100

    Schema update

--- src/like.rs
+++ src/like.rs
@@ -1,48 +1,53 @@
-pub type Likes = Response<Like>;
+pub fn list_likes(_tweet_id: Uuid, conn: &DBPooledConnection) -> Result<Likes, Error> {
+    use crate::schema::likes::dsl::*;
 
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Like {
-    pub id: String,
-    pub created_at: DateTime<Utc>,
+    let _likes: Vec<LikeDB> = match likes
+        .filter(tweet_id.eq(_tweet_id))
+        .order(created_at.desc())
+        .load::<LikeDB>(conn)
+    {
+        Ok(lks) => lks,
+        Err(_) => vec![],
+    };
+
+    Ok(Likes {
+        results: _likes
+            .into_iter()
+            .map(|l| l.to_like())
+            .collect::<Vec<Like>>(),
+    })
 }
 
-impl Like {
-    pub fn new() -> Self {
-        Self {
-            id: Uuid::new_v4().to_string(),
-            created_at: Utc::now(),
-        }
-    }
-}
+pub fn create_like(_tweet_id: Uuid, conn: &DBPooledConnection) -> Result<Like, Error> {
+    use crate::schema::likes::dsl::*;
 
-/// list last 50 likes from a tweet `/tweets/{id}/likes`
-#[get("/tweets/{id}/likes")]
-pub async fn list(path: Path<(String,)>) -> HttpResponse {
-    // TODO find likes by tweet ID and return them
-    let likes = Likes { results: vec![] };
-
-    HttpResponse::Ok()
-        .content_type(APPLICATION_JSON)
-        .json(likes)
-}
-
-/// add one like to a tweet `/tweets/{id}/likes`
-#[post("/tweets/{id}/likes")]
-pub async fn plus_one(path: Path<(String,)>) -> HttpResponse {
-    // TODO add one like to a tweet
     let like = Like::new();
+    let _ = diesel::insert_into(likes)
+        .values(like.to_like_db(_tweet_id))
+        .execute(conn);
 
-    HttpResponse::Created()
-        .content_type(APPLICATION_JSON)
-        .json(like)
+    Ok(like)
 }
 
-/// remove one like from a tweet `/tweets/{id}/likes`
-#[delete("/tweets/{id}/likes")]
-pub async fn minus_one(path: Path<(String,)>) -> HttpResponse {
-    // TODO remove one like to a tweet
-    HttpResponse::NoContent()
-        .content_type(APPLICATION_JSON)
-        .await
-        .unwrap()
+pub fn delete_like(_tweet_id: Uuid, conn: &DBPooledConnection) -> Result<(), Error> {
+    use crate::schema::likes::dsl::*;
+
+    let _likes = list_likes(_tweet_id, conn);
+
+    let like = match &_likes {
+        Ok(_likes) if !_likes.results.is_empty() => _likes.results.first(),
+        _ => None,
+    };
+
+    if like.is_none() {
+        return Ok(());
+    }
+
+    let like_id = Uuid::from_str(like.unwrap().id.as_str()).unwrap();
+
+    let res = diesel::delete(likes.filter(id.eq(like_id))).execute(conn);
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(err),
+    }
 }
